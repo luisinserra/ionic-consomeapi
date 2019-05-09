53e6071ea4af46e179d360dc1066e9c1e4b8f224
luisinserra <luisinserra@yahoo.com.br>
2019-05-09 17:46:55 -0300

    1 file modified

--- app/main.ts
+++ app/main.ts
@@ -7,7 +7,7 @@ import { AppModule } from './app.module';
 platformBrowserDynamic().bootstrapModule(AppModule).then(ref => {
   // Ensure Angular destroys itself on hot reloads.
   if (window['ngRef']) {
-    window['ngRef'].destroy();
+    window['ngRef'].destroy(); 
   }
   window['ngRef'] = ref;
 
