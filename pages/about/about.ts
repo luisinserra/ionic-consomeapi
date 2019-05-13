716fe1afe0939ad951451e0aec90036ece51f66d
luisinserra <luisinserra@yahoo.com.br>
2019-05-12 23:43:11 -0300

    2 files modified

--- pages/about/about.ts
+++ pages/about/about.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { AlertController } from 'ionic-angular';
 
 @Component({
   selector: 'page-about',
@@ -7,8 +8,38 @@ import { NavController } from 'ionic-angular';
 })
 export class AboutPage {
 
-  constructor(public navCtrl: NavController) {
+  tSenha: string;
+  private alertCtrl;
+  tLogin: string;
+  tParm: string;
+  oo: string;
 
+  constructor(public navCtrl: NavController, alertCtrl : AlertController) {
+    this.alertCtrl = alertCtrl;
+    this.tParm="Veja!";
+  }
+
+  handleChange = () => {
+    console.log("mudou");
+  }
+
+  setLogin = (parm) => {
+    this.tLogin=parm;
+  }
+
+  setSenha = (parm) => {
+    this.tSenha = parm;
+  }
+
+  clicou = () => {
+    this.oo="MMM";
+      let msg="Botão pressionado! para "+this.tLogin+", senha = "+this.tSenha;
+      const alert = this.alertCtrl.create({
+        title: 'Vamos nessa!',
+        subTitle: msg,
+        buttons: ['Tá bom']
+      });
+      alert.present();
   }
 
 }
