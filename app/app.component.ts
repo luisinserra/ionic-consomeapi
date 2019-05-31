7d7e700634b8c889d804ff9d1325d4c7cdb31280
luisinserra <luisinserra@yahoo.com.br>
2019-05-31 00:51:16 -0300

    1 file modified

--- app/app.component.ts
+++ app/app.component.ts
@@ -12,7 +12,7 @@ import { DatabasePrivider } from '../providers/database/database';
 export class MyApp {
   rootPage:any = TabsPage;
 
-  constructor(platform: Platform) {
+  constructor(platform: Platform, dbProvider: DatabaseProvider) {
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
